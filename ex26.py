afdccc596b1ad837a15af7acd7b5569948114ab5
Francisco Anacleto de Albuquerque <104237733+faalb@users.noreply.github.com>
2023-04-21 21:21:29 -0300

    Update and rename ex1038.py to ex26.py

--- ex26.py
+++ ex26.py
@@ -1,15 +1,3 @@
-# Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir,
-# calcule e mostre o valor da conta a pagar.
-
-# Entrada
-# O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
-
-# Saída
-# O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.
-
-# Exemplo de Entrada	Exemplo de Saída
-# 3 2Total:             R$ 10.00
-
 if __name__ == '__main__':
     v = input('Digite o codigo do item e a quantidade separado por espaço: ').split()
     c, q = int(v[0]), int(v[1])
