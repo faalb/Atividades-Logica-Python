f2b1f3f3f221fd266d1a4040134c9ceb01a8f768
Francisco Anacleto de Albuquerque <104237733+faalb@users.noreply.github.com>
2023-04-21 21:23:30 -0300

    Update and rename ex1041.py to ex28.py

--- ex28.py
+++ ex28.py
@@ -1,19 +1,3 @@
-# Leia 2 valores com uma casa decimal (x e y), que devem representar as coordenadas de um ponto em um plano. A seguir,
-# determine qual o quadrante ao qual pertence o ponto, ou se está sobre um dos eixos cartesianos ou na origem (x = y = 0).
-
-# Se o ponto estiver na origem, escreva a mensagem “Origem”.
-
-# Se o ponto estiver sobre um dos eixos escreva “Eixo X” ou “Eixo Y”, conforme for a situação.
-
-# Entrada
-# A entrada contem as coordenadas de um ponto.
-
-# Saída
-# A saída deve apresentar o quadrante em que o ponto se encontra.
-
-# Exemplo de Entrada	Exemplo de Saída
-# 4.5 -2.2              Q4
-
 if __name__ == '__main__':
     v = input('Digite dois valores separando-os com um espaço: ').split()
     x, y = float(v[0]), float(v[1])
